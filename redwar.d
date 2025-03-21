54a18a56a45671ebb236c60e8405be2c7a15e144
nguyenuyen1991 <nguyenuyen19912@gmail.com>
2025-03-21 09:24:19 +0700

    Update redwar.d

--- redwar.d
+++ redwar.d
@@ -1,1 +1,1 @@
-Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
+AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
