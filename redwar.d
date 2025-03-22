05623090f9dea4859158a13a20d52e692d22420a
nguyenuyen1991 <nguyenuyen19912@gmail.com>
2025-03-22 09:23:59 +0700

    Update redwar.d

--- redwar.d
+++ redwar.d
@@ -1,1 +1,1 @@
-AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
+A true programmer never blames the compiler.AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
