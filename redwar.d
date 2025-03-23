b7b784a6e751452dc9bc3e2dc4c837c8159ba95b
nguyenuyen1991 <nguyenuyen19912@gmail.com>
2025-03-23 09:24:34 +0700

    Update redwar.d

--- redwar.d
+++ redwar.d
@@ -1,1 +1,1 @@
-A true programmer never blames the compiler.AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
+The cloud is just someone else’s downtime.A true programmer never blames the compiler.AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
