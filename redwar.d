339c77d8f885a866a0142642b4f2ef553fc752fc
nguyenuyen1991 <nguyenuyen19912@gmail.com>
2025-03-24 09:24:32 +0700

    Update redwar.d

--- redwar.d
+++ redwar.d
@@ -1,1 +1,1 @@
-The cloud is just someone else’s downtime.A true programmer never blames the compiler.AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
+Documentation is like a love letter to your future self.The cloud is just someone else’s downtime.A true programmer never blames the compiler.AI won’t take your job, but another programmer will.Refactor or regret later.Never trust a programmer who claims their code is bug-free.redwar.d
